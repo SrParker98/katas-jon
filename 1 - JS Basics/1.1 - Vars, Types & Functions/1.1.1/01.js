7cd9792b2e9ae56ab58f7410089329d289cae394
SrParker98 <116755291+SrParker98@users.noreply.github.com>
2022-12-12 18:38:51 +0100

    Add files via upload

---
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/01.js
@@ -0,0 +1,3 @@
+const nombre = 'sergi';
+console.log(nombre);
+// Crea una variable con tu nombre y muestrala por consola usando console.log
---
+++ 1 - JS Basics/1.1 - Vars, Types & Functions/1.1.1/02.js
@@ -0,0 +1,6 @@
+// Encuentra el error en este código
+
+let a = 4;
+a = 5;
+
+console.log(a);
